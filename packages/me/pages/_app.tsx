003b296535628b0d108e6e36d25de8f48cf7fbef
wachira_Dev <ewachira254@gmail.com>
2020-11-14 17:55:57 +0300

    ⭐ @bywachira/me: add metatags

--- packages/me/pages/_app.tsx
+++ packages/me/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import { AppContainer } from "../styled-components/styles";
 import "../static/index.css";
@@ -9,9 +10,44 @@ type Props = {
 
 const MyApp: React.FC<Props> = ({ Component, pageProps }) => {
   return (
-    <AppContainer>
-      <Component {...pageProps} />
-    </AppContainer>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <link rel="icon" type="image/png" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <meta name="title" content={"Erick Wachira"} />
+        <meta name="description" content={"Summary bywachira"} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://bywachira.com" />
+        <meta property="og:title" content={"Erick Wachira"} />
+        <meta property="og:description" content="Summary bywachira" />
+        <meta
+          property="og:image"
+          content={
+            "https://res.cloudinary.com/duoxba7n1/image/upload/v1597872359/blog/logo2.png"
+          }
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={""} />
+        <meta property="twitter:title" content="Erick Wachira" />
+        <meta property="twitter:description" content={"Summary bywachira"} />
+        <meta
+          property="twitter:image"
+          content={
+            "https://res.cloudinary.com/duoxba7n1/image/upload/v1597872359/blog/logo2.png"
+          }
+        />
+        <title>Erick Wachira</title>
+      </Head>
+      <AppContainer>
+        <Component {...pageProps} />
+      </AppContainer>
+    </>
   );
 };
 
